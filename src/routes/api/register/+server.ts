3436085d5e253a9050aae4cd0d14c04f373d902d
Nachop51 <nachoperalta0@gmail.com>
2024-01-26 15:50:45 -0300

    simple mongo connection

---
+++ src/routes/api/register/+server.ts
@@ -0,0 +1,21 @@
+import type { RequestHandler } from './$types'
+import { MongoClient } from 'mongodb'
+import { MONGO_URI } from '$env/static/private'
+
+const client = new MongoClient(MONGO_URI)
+
+export const GET: RequestHandler = async () => {
+	try {
+		const database = client.db('nachort')
+		const users = database.collection('users')
+		// Query for a movie that has the title 'Back to the Future'
+		const user = users.find({})
+		for await (const doc of user) {
+			console.log(doc)
+		}
+	} catch (error) {
+		console.error(error)
+	}
+
+	return new Response()
+}
